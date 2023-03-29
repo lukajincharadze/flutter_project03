cc48b0f262170c344ae0949ce994d6a33c5c547a
Luka Jincharadze <lukajinchara@gmail.com>
2023-03-29 10:59:59 +0400

    commit
    when you click on photo, you'll get sound of that specific animal sound

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
 
   final audioplayer = AudioPlayer();
 
-  Future setAudio({required String path})  {
+  Future setAudio({required String path}) {
     return audioplayer.setSource(AssetSource(path));
   }
 
@@ -20,21 +20,127 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () async {
-                await setAudio(path: "sounds/dogbark.wav");
-                 audioplayer.play(audioplayer.source!);
-              },
-              child: Container(
-                  child: Image.asset("assets/images/dogphoto.jpg"),
-                  width: 400,
-                  height: 400,
-              ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Text(
+                    'Click Photos to get sound',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/dogbark.wav");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/dogphoto.jpg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/catmeow.wav");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/cat.jpg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/tigerroar.mp3");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/tiger.jpg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/lionroar.wav");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/lion.jpg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/horsesound.wav");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/horse.jpeg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () async {
+                            await setAudio(path: "sounds/cowmoo.wav");
+                            audioplayer.play(audioplayer.source!);
+                          },
+                          child: Container(
+                            child: Image.asset("assets/images/cow.jpg"),
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
